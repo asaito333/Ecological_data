72b75dec083e9ee746adf10ce5dc6d474a930186
Asumi Saito <asumisaito1@gmail.com>
2017-07-18 12:19:50 -0700

    Update SQL practice.sql

--- SQL practice.sql
+++ SQL practice.sql
@@ -1,5 +1,7 @@
+/* Codeacademy Table transforamtion */
+
 select a.dep_month,
-			 a.dep_day_of_week,
+	a.dep_day_of_week,
        AVG(a.flight_distance) as average_distance
 from(
   select dep_month,
@@ -7,7 +9,17 @@ from(
   			 dep_date,
   			 sum(distance) as flight_distance
   from Flights
-GROUP BY 1,2,3
+GROUP BY 1,2,3  /* this group 1,2,3 refer to the column # for each column from dep_month */
 ) a
 GROUP BY 1,2
 ORDER BY 1,2;
+
+/* assigne a sequential number to id or whatever using inner query*/
+select origin, id,
+(select count(*)
+from flights f
+where f.id < flights.id
+and f.origin=flights.origin) +1
+as flight_sequence_number
+from Flights
+;
